72f5ca5d8d732bebb1cc47a2bd4f8f87b1a7b881
Hidayatullah Gustiawan Adi <adihidayatullah96.ah@gmail.com>
2022-06-14 15:02:35 +0700

    tambah multer untuk file

--- routes/router.js
+++ routes/router.js
@@ -2,12 +2,18 @@ const express   = require('express')
 // const { route } = require('express/lib/application');
 const router    = express.Router();
 const sampleController = require('../controllers/sampleController')
+const foodController = require('../controllers/foodsController')
+
 router.get('/', sampleController.methodGet)
-
 router.post('/', sampleController.methodPost)
-
 router.put('/', sampleController.methodPut)
-
 router.delete('/', sampleController.methodDelete)
+
+// foods
+router.post('/foods', foodController.methodPost)
+router.get('/foods', foodController.methodGet)
+router.get('/foods/:id', foodController.methodGetId)
+router.put('/foods/:id', foodController.methodPut)
+router.delete('/foods/:id', foodController.methodDelete)
 
 module.exports = router
